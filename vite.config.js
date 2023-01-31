a1793bebd451d704c3f5586db1318f5306bc4906
BamoJ <94501520+BamoJ@users.noreply.github.com>
2023-01-31 11:30:17 +0700

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import eslintPlugin from 'vite-plugin-eslint'
+import glsl from 'vite-plugin-glsl';
+
+// vite.config.js
+export default defineConfig({
+  plugins: [eslintPlugin, glsl()],
+  server: {
+    host: 'localhost',
+    cors: '*',
+    hmr: {
+      host: 'localhost',
+      protocol: 'ws',
+    },
+  },
+  build: {
+    minify: true,
+    manifest: true,
+    rollupOptions: {
+      input: './src/main.js',
+      output: {
+        format: 'umd',
+        entryFileNames: 'main.js',
+        esModule: false,
+        compact: true,
+        globals: {
+          jquery: '$',
+        },
+      },
+      external: ['jquery'],
+    },
+  },
+})
